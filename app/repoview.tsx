4fa3d1b7d65db046f0fb2948a5bca9163fc948e2
Ryan Dalton <rdalton2405@gmail.com>
2026-07-11 14:20:57 -0400

    Scaffolding for repoviewer

--- app/repoview.tsx
+++ app/repoview.tsx
@@ -1,23 +1,47 @@
 import { useState } from "react";
 import { Repository } from "./page";
 
-export default function RepoView({curRepo} : {curRepo: Repository}) {
+export default function RepoView({ curRepo }: { curRepo: Repository }) {
     const [activeSection, setActiveSection] = useState<string | null>(null);
-    
+
     const openSection = (section: string) => {
         setActiveSection(section)
     };
-    
+
     return (
         <div>
             <h1>{curRepo.nameWithOwner}</h1>
             <h2>{curRepo.description}</h2>
             <a href={curRepo.url} target="_blank" rel="noopener noreferrer">Repo link</a>
-            
+
             <button onClick={() => openSection("Issues")}>View Issues</button>
             <button onClick={() => openSection("PRs")}>View PRs</button>
             <button onClick={() => openSection("Readme")}>View Readme</button>
             <button onClick={() => openSection("Codebase")}>View Codebase</button>
+
+            {activeSection === "Issues" && (
+                <div className="section">
+
+                </div>
+            )}
+
+            {activeSection === "PRs" && (
+                <div className="section">
+
+                </div>
+            )}
+
+            {activeSection === "Readme" && (
+                <div className="section">
+
+                </div>
+            )}
+
+            {activeSection === "Codebase" && (
+                <div className="section">
+
+                </div>
+            )}
 
 
         </div>
