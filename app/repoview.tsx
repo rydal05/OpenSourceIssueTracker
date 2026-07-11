355f7fa5444010860f483d446b84936b194ab626
Ryan Dalton <rdalton2405@gmail.com>
2026-07-11 14:10:00 -0400

    repoview formatting

--- app/repoview.tsx
+++ app/repoview.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { Repository } from "./page";
 
 export default function RepoView({curRepo} : {curRepo: Repository}) {
+    const [activeSection, setActiveSection] = useState<string | null>(null);
+    
+    const openSection = (section: string) => {
+        setActiveSection(section)
+    };
+    
     return (
         <div>
+            <h1>{curRepo.nameWithOwner}</h1>
+            <h2>{curRepo.description}</h2>
+            <a href={curRepo.url} target="_blank" rel="noopener noreferrer">Repo link</a>
+            
+            <button onClick={() => openSection("Issues")}>View Issues</button>
+            <button onClick={() => openSection("PRs")}>View PRs</button>
+            <button onClick={() => openSection("Readme")}>View Readme</button>
+            <button onClick={() => openSection("Codebase")}>View Codebase</button>
+
 
         </div>
     );
