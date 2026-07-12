ce4bbb6945ac81254ced77b9c5bd863db288e8c6
satalight <114524244+satasatalight@users.noreply.github.com>
2026-07-11 21:11:11 -0400

    hide intro

--- app/intro.tsx
+++ app/intro.tsx
@@ -1,8 +1,8 @@
-export default function Intro({setRawSearch}: 
+export default function Intro({setRawSearch, setGeminiResult}: 
     {setRawSearch: React.Dispatch<React.SetStateAction<string>>}) {
 
     return (
-        <div className="z-10 flex py-10 px-10 flex-col text-3xl absolute w-md bg-white border-1 rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="hidden z-10 flex py-10 px-10 flex-col text-3xl absolute w-md bg-white border-1 rounded-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <label className="pb-3 pt-4">What level of programming skill would you say you're at?</label>
             <input className="border-1 my-3 rounded-lg px-2" type="text" placeholder="e.g., Intermediate"></input>
 
